b5acef0fe956b687e31cf3e14c786f0511f57c03
eduardodasilvauninove-create <eduardodasilva.uninove@gmail.com>
2026-05-14 17:50:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,13 +14,19 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
     dedupe: ["react", "react-dom"],
   },
+
   optimizeDeps: {
     include: ["react", "react-dom"],
   },
